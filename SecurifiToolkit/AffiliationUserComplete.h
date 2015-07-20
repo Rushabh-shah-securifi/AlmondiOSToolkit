6190fc7fe4f1afdf48301cc19c3845edda127372
Matthew Sinclair-Day <msd@mingfu.tw>
2015-07-20 09:53:48 +0800

    Building a new affiliation view controller

--- SecurifiToolkit/AffiliationUserComplete.h
+++ SecurifiToolkit/AffiliationUserComplete.h
@@ -8,13 +8,28 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM(unsigned int, AffiliationUserCompleteFailureCode) {
+    AffiliationUserCompleteFailureCode_systemDown = 1,
+    AffiliationUserCompleteFailureCode_invalidCode = 2,
+    AffiliationUserCompleteFailureCode_invalidCode2 = 3,
+    AffiliationUserCompleteFailureCode_alreadyLinked = 4,
+    AffiliationUserCompleteFailureCode_loginAgain = 5,
+    AffiliationUserCompleteFailureCode_loginAgain2 = 6,
+    AffiliationUserCompleteFailureCode_loginAgain3 = 7,
+};
+
 @interface AffiliationUserComplete : NSObject
-@property (nonatomic) BOOL isSuccessful;
-@property (nonatomic, copy) NSString *almondplusName;
-//8 bytes
-@property (nonatomic, copy)NSString *almondplusMAC;
-@property (nonatomic, copy)NSString *reason;
-@property (nonatomic, copy)NSString *wifiSSID;
-@property (nonatomic, copy)NSString *wifiPassword;
-@property int reasonCode;
+@property(nonatomic) BOOL isSuccessful;
+@property(nonatomic, copy) NSString *almondplusName;
+
+//8 bytes; use formattedAlmondPlusMac for human friendly string
+@property(nonatomic, copy) NSString *almondplusMAC;
+
+@property(nonatomic, copy) NSString *reason;
+@property(nonatomic, copy) NSString *wifiSSID;
+@property(nonatomic, copy) NSString *wifiPassword;
+@property(nonatomic) enum AffiliationUserCompleteFailureCode reasonCode;
+
+- (NSString *)formattedAlmondPlusMac;
+
 @end
--- SecurifiToolkit/AffiliationUserComplete.m
+++ SecurifiToolkit/AffiliationUserComplete.m
@@ -9,4 +9,27 @@
 #import "AffiliationUserComplete.h"
 
 @implementation AffiliationUserComplete
+
+- (NSString *)formattedAlmondPlusMac {
+    return [self convertDecimalToMAC:self.almondplusMAC];
+}
+
+- (NSString *)convertDecimalToMAC:(NSString *)decimalString {
+    //Step 1: Conversion from decimal to hexadecimal
+    DLog(@"%llu", (unsigned long long) [decimalString longLongValue]);
+    NSString *hexIP = [NSString stringWithFormat:@"%llX", (unsigned long long) [decimalString longLongValue]];
+
+    NSMutableString *wifiMAC = [[NSMutableString alloc] init];
+    //Step 2: Divide in pairs of 2 hex
+    for (NSUInteger i = 0; i < [hexIP length]; i = i + 2) {
+        NSString *ichar = [NSString stringWithFormat:@"%c%c:", [hexIP characterAtIndex:i], [hexIP characterAtIndex:i + 1]];
+        [wifiMAC appendString:ichar];
+    }
+
+    [wifiMAC deleteCharactersInRange:NSMakeRange([wifiMAC length] - 1, 1)];
+
+    DLog(@"WifiMAC: %@", wifiMAC);
+    return wifiMAC;
+}
+
 @end
