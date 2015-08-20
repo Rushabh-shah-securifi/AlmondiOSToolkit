45eb350a19cdfe56459eed325a9f44104bbade63
Matthew Sinclair-Day <msd@mingfu.tw>
2015-08-20 14:26:13 +0800

    clean up

--- SecurifiToolkit/AppKit/NSData+Securifi.m
+++ SecurifiToolkit/AppKit/NSData+Securifi.m
@@ -11,6 +11,13 @@
 @implementation NSData (Securifi)
 
 - (NSString *)securifiDecryptPasswordForAlmond:(NSString *)almondMac almondUptime:(NSString *)almondUptimeInt {
+    if (!almondMac) {
+        return nil;
+    }
+    if (!almondUptimeInt) {
+        return nil;
+    }
+
     // Compute the IV
     const char *MAC = [almondMac cStringUsingEncoding:NSUTF8StringEncoding];
     const char *UPTIME = [almondUptimeInt cStringUsingEncoding:NSUTF8StringEncoding];;
@@ -27,15 +34,11 @@
     char KEY[] = {0x6e, (char) 0xcc, (char) 0x94, (char) 0xed, 0x6a, (char) 0x90, 1, 0x3d, 0x30, (char) 0xaf, 0x52, 0xd, 0x18, 0x77, 0x44, 0x2f};
 
     // Decrypt
-    NSData *decrypted = [self securifiDecryptWithKey:KEY iv:IV];
+    NSData *decrypted = [self securifiInternalAesOp:kCCDecrypt payload:self key:KEY iv:IV];
 
     // Convert back to string; we expect the plain text password to be UTF-8 encoded
     NSString *decrypted_str = [[NSString alloc] initWithData:decrypted encoding:NSUTF8StringEncoding];
     return decrypted_str;
-}
-
-- (NSData *)securifiDecryptWithKey:(char[])key iv:(char[])iv {
-    return [self securifiInternalAesOp:kCCDecrypt payload:self key:key iv:iv];
 }
 
 - (NSData *)securifiInternalAesOp:(const CCOperation)op payload:(const NSData *)payload key:(const char[])key iv:(const char[])iv {
