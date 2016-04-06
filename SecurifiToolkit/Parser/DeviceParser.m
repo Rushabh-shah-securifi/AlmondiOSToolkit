61f89e1795347362f533281772ba00e0da873309
Rushabh-shah-securifi <shailesh.shah@securifi.com>
2016-04-06 19:16:01 +0530

    dummy device

--- SecurifiToolkit/Parser/DeviceParser.m
+++ SecurifiToolkit/Parser/DeviceParser.m
@@ -153,25 +153,25 @@
 
 -(void)parseDeviceListAndDynamicDeviceResponse:(id)sender{
     NSLog(@"parseDeviceListAndDynamicDeviceResponse");
-    NSNotification *notifier = (NSNotification *) sender;
-    NSDictionary *dataInfo = [notifier userInfo];
-    if (dataInfo == nil || [dataInfo valueForKey:@"data"]==nil ) {
-        return;
-    }
+//    NSNotification *notifier = (NSNotification *) sender;
+//    NSDictionary *dataInfo = [notifier userInfo];
+//    if (dataInfo == nil || [dataInfo valueForKey:@"data"]==nil ) {
+//        return;
+//    }
     SecurifiToolkit *toolkit = [SecurifiToolkit sharedInstance];
-    SFIAlmondPlus *almond = [toolkit currentAlmond];
-    BOOL local = [toolkit useLocalNetwork:almond.almondplusMAC];
+//    SFIAlmondPlus *almond = [toolkit currentAlmond];
+//    BOOL local = [toolkit useLocalNetwork:almond.almondplusMAC];
     NSDictionary *payload;
-    if(local){
-        payload = [dataInfo valueForKey:@"data"];
-    }else{
-        payload = [[dataInfo valueForKey:@"data"] objectFromJSONData];
-    }
-//    payload = [self parseJson:@"DeviceListResponse"];
+//    if(local){
+//        payload = [dataInfo valueForKey:@"data"];
+//    }else{
+//        payload = [[dataInfo valueForKey:@"data"] objectFromJSONData];
+//    }
+    payload = [self parseJson:@"DeviceListResponse"];
     NSLog(@"devices - payload: %@", payload);
-    BOOL isMatchingAlmondOrLocal = ([[payload valueForKey:ALMONDMAC] isEqualToString:almond.almondplusMAC] || local) ? YES: NO;
-    if(!isMatchingAlmondOrLocal) //for cloud
-        return;
+//    BOOL isMatchingAlmondOrLocal = ([[payload valueForKey:ALMONDMAC] isEqualToString:almond.almondplusMAC] || local) ? YES: NO;
+//    if(!isMatchingAlmondOrLocal) //for cloud
+//        return;
     
     if([[payload valueForKey:COMMAND_TYPE] isEqualToString:DEVICE_LIST]){
         NSDictionary *devicesPayload = payload[DEVICES];
